b7776aabd15e215186f5173d03a2ff880787efcb
Drew Chase <dcmanproductions@gmail.com>
2023-07-11 18:21:03 -0400

    added packages page

---
+++ packages/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    $title = "Packages";
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/header.php"
+    ?>
+    <link rel="stylesheet" href="/assets/css/min/home.min.css">
+</head>
+
+<body>
+    <?php
+    $page = 2;
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/nav.php";
+    ?>
+
+    <?php include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/footer.php"; ?>
+
+</body>
+
+</html>
