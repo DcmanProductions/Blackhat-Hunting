3db1d5a87de71e7e7b7429846714f916dba7528b
Drew Chase <dcmanproductions@gmail.com>
2023-07-11 18:20:58 -0400

    added gallery page

---
+++ gallery/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    $title = "Gallery";
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/header.php"
+    ?>
+    <link rel="stylesheet" href="/assets/css/min/home.min.css">
+</head>
+
+<body>
+    <?php
+    $page = 3;
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/nav.php";
+    ?>
+
+
+<?php include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/footer.php"; ?>
+
+</body>
+
+</html>
